54bab238c02701c9c9268afedb07e7e4bc32f6b8
Akshuu1 <akshata.14feb@gmail.com>
2025-07-28 00:05:03 +0530

    vercel error fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a3Rzcmtxcnp3ZXhnZWZ3Z2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NTczMzQsImV4cCI6MjA2OTEzMzMzNH0.WZ4vOcoBb_WBhrDctUL_LyKy9wf0ApSSwyJJ3Dg8tLQ"
+const supabaseAnonKey = "https://hyktsrkqrzwexgefwgah.supabase.co"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
