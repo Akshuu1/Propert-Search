801292fe8bf5e546143406621a325a41f86b1d33
Akshuu1 <akshata.14feb@gmail.com>
2025-07-28 00:53:42 +0530

    fixed a minor issuue

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -2,7 +2,7 @@ import AuthClient from "./AuthClient";
 
 export const metadata = {
   title: "Domora - Auth",
-  description: "Authentication page for Domora",
+  description: "Authentication page for Espiro",
 };
 
 export default function AuthPage() {
--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a3Rzcmtxcnp3ZXhnZWZ3Z2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NTczMzQsImV4cCI6MjA2OTEzMzMzNH0.WZ4vOcoBb_WBhrDctUL_LyKy9wf0ApSSwyJJ3Dg8tLQ"
-const supabaseAnonKey = "https://hyktsrkqrzwexgefwgah.supabase.co"
+const supabaseUrl = "https://hyktsrkqrzwexgefwgah.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a3Rzcmtxcnp3ZXhnZWZ3Z2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NTczMzQsImV4cCI6MjA2OTEzMzMzNH0.WZ4vOcoBb_WBhrDctUL_LyKy9wf0ApSSwyJJ3Dg8tLQ"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
